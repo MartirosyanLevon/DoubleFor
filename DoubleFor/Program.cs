e09dffa53af02d356f347f78101215a5c55bfb5c
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-09 12:49:26 +0400

    relise one

---
+++ DoubleFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" i = {0} ", i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
